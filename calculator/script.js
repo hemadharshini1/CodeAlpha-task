2d16ca06ea84e657c8a025a7319baaf7ae65d8e7
hemadharshini1 <155095113+hemadharshini1@users.noreply.github.com>
2025-03-11 21:15:54 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,13 @@
+function appendValue(value) {
+    document.getElementById('screen').value += value;
+}
+function clearScreen() {
+    document.getElementById('screen').value = '';
+}
+function calculate() {
+    try {
+        document.getElementById('screen').value = eval(document.getElementById('screen').value);
+    } catch (error) {
+        alert('Invalid Expression');
+    }
+}
